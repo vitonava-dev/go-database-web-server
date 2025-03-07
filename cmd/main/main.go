3abf1444f368e5a35e7415ce40ec53d562efa409
vitonava-dev <vic.nb.99@hotmail.com>
2025-03-06 22:23:25 -0600

    feat: Created controller, main, models

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,4 +1,17 @@
 package main
 
-import "fmt"
-fmt.Println("Hello, World!")
+import (
+	"go-database-webserver/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", nil))
+}
