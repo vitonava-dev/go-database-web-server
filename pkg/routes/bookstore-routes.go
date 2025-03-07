62f55c56dab92db2bce37f6f1fb83c4abaf8cebf
vitonava-dev <vic.nb.99@hotmail.com>
2025-03-06 18:38:53 -0600

    feat: database, routes, utils ready

---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"go-database-webserver/pkg/controllers"
+
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
